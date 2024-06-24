3e53bf1fe8ff77db280a8694c6d989fd731e20f7
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-06-24 14:11:02 +0200

    Add healthcheck for plugin

--- lua/notisnisse/health.lua
+++ lua/notisnisse/health.lua
@@ -1,7 +1,31 @@
 local M = {}
 
 M.check = function()
-	-- FIXME: Make sure the path is writable
+	vim.health.start("Sqlite3")
+
+	-- Check if `sqlite3` is installed
+	if vim.fn.executable("sqlite3") == 0 then
+		vim.health.error("sqlite3")
+	else
+		-- Run `sqlite3 --version` to get the version of dotnet
+		local handle = io.popen("sqlite3 --version")
+
+		-- If the output is nil then report an error (this should never happen)
+		if handle == nil then
+			vim.health.error("error on attempting to read `sqlite3 --version`")
+			return
+		end
+
+		-- Read the result of running `sqlite3 --version`
+		local result = handle:read("*a")
+		handle:close()
+
+		-- Get the first word of the output (i.e. the version)
+		local version = result:match("%S+")
+
+		-- Report the version of `sqlite3`
+		vim.health.ok("`sqlite3` found " .. version)
+	end
 end
 
 return M
